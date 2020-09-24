763912ed187f252a5e874f89baa331e7b68c361d
trilambda122 <trilambda122>
2020-09-23 21:34:40 -0500

    added the inquirer module

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const employee = require('./lib/employee.js');
-const manager = require('./lib/manager.js');
-const engineer = require('./lib/engineer.js');
+function run() {
+    const employee = require('./lib/employee.js');
+    const manager = require('./lib/manager.js');
+    const engineer = require('./lib/engineer.js');
 
 
-let fred = new employee('fred', '01', 'fred@info.com');
-let heather = new manager('heather', '02', 'heather@info', '814.2485341');
-let geogre = new engineer("george", "05", "george@jungle", 'github');
+    let fred = new employee('fred', '01', 'fred@info.com');
+    let heather = new manager('heather', '02', 'heather@info', '814.2485341');
+    let geogre = new engineer("george", "05", "george@jungle", 'github');
 
 
-console.log(heather.officeNumber);
+    console.log(heather.officeNumber);
 
-console.log(geogre);
+    console.log(geogre);
 
-console.log(fred.getRole());
+    console.log(fred.getRole());
+}
+
+run();
